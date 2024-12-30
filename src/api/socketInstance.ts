38fa4b9e64242c33a8712541d1dc0ed407830b33
Aayush Solanki <aayushsolanki40@gmail.com>
2024-12-30 23:06:40 +0530

    feat: added transport layer for socket instance

--- src/api/socketInstance.ts
+++ src/api/socketInstance.ts
@@ -1,5 +1,9 @@
 import { io } from "socket.io-client";
 
-const socketInstance = io("http://localhost:3050", { withCredentials: true, secure: true });
+const socketInstance = io("http://localhost:3050", {
+  withCredentials: true,
+  secure: true,
+  transports: ["websocket"],
+});
 
 export default socketInstance;
